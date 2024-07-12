882d37ac299b9ef96ba9a4bae6bd531936b68e27
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-07-13 00:26:15 +0300

    Shared Tailwindcss Config File

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //apps content
+    "./src/**/*.{html,js,ts,tsx,jsx}",
+    //packages content
+    "../../src/**/*.{html,js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
